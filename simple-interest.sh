3c858fbc0ef89278d41d884457ba46d356c2f0e8
Sugirtharajendran <153611707+Sugirtharajendran@users.noreply.github.com>
2024-03-29 20:22:17 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,2 @@
+simple-interest.sh this file is named as simple interest as mentioned in the pdf.
+just use this repo as a review and use it to add some changes and you are also welcome to create any pull request to made any modifications in it.
